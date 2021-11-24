902903ff7109a9e855fccef7581c1c04464bba2d
Joel Kovalcson <joelkovalcson@gmail.com>
2021-11-24 14:43:50 -0500

    Started working on skeleton for generating the feed

--- assets/js/modules/feed.js
+++ assets/js/modules/feed.js
@@ -1,12 +1,33 @@
-export var feed = {
-    mainElement: "",
-    setup(el) {
-        this.mainElement = el
+const storageString = "gh-at-a-glance/feed";
+
+export let feed = {
+    mainElement: null,
+    storage: null,
+    setup: function(el = null) {
+        this.mainElement = el;
+        this.storage = JSON.parse(localStorage.getItem(storageString));
+        this.updateFeed();
     },
-    loadStorage() {
-        // TODO
+    saveStorage: function() {
+        localStorage.setItem(storageString, JSON.stringify(this.storage.current));
     },
-    saveStorage() {
-        // TODO
+    addFeed: function(item) {
+        this.storage.push(item);
+        this.updateFeed();
+    },
+    removeFeed: function(item) {
+        // TODO 
+    },
+    updateFeed: function() {
+        // Clear Feed
+        this.mainElement.innerHTML = "";
+        // Generate section for each item if there's something different
+        if(this.storage == null) return;
+        this.storage.forEach(item => {
+            // Generate Div structure and append
+            let itemDiv = document.createElement("div");
+            
+            this.mainElement.append(itemDiv);
+        });
     }
 }
