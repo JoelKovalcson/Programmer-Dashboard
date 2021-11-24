28c19dcfd53bd75412e4a8be8401630e2bec762a
Joel Kovalcson <joelkovalcson@gmail.com>
2021-11-24 14:42:34 -0500

    Fixed a couple bugs so news articles are displayed

--- assets/js/modules/news.js
+++ assets/js/modules/news.js
@@ -1,24 +1,26 @@
 const url = `https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty`
 
 
-export function getNews (url) {
+export function getNews (newsEl) {
     fetch(url)
     .then(response => response.json())
     .then(data => {
+        newsEl.innerHTML = "";
         for(var i = 0 ; i < data.length; i++) {
             if(i < 25) {
-                let itemUrl = `https://hacker-news.firebaseio.com/v0/item/${element[i]}.json?print=pretty`
+                let itemUrl = `https://hacker-news.firebaseio.com/v0/item/${data[i]}.json?print=pretty`
                 fetch(itemUrl)
                 .then(response => response.json())
                 .then(data => {
-                    populateNews(data)
+                    populateNews(data, newsEl)
                 })
             }    
         }
     })
 }
 
-export function populateNews(data) {
+export function populateNews(data, newsEl) {
+    
     let div = document.createElement("div")
     let h3 = document.createElement("h3")
     let a = document.createElement("a")
@@ -26,8 +28,6 @@ export function populateNews(data) {
     h3.textContent = data.title
     a.textContent = "Visit this Article"
     a.setAttribute("href", data.url)
-    popularEl.appendChild(div)
+    newsEl.appendChild(div)
     div.append(h3, a)
 }
-
-getNews()
